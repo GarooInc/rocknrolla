99eeafdd33694752b0c653be1e243ffa26c455ae
Jimena Hernández <her21199@uvg.edu.gt>
2024-09-23 09:15:37 -0600

    updates

--- components/Group/Group.jsx
+++ components/Group/Group.jsx
@@ -5,7 +5,7 @@ const Group = ({ cover, logo, secondary }) => {
   return (
     <div
       style={{ backgroundImage: `url(${cover})` }}
-      className='group bg-cover bg-center bg-no-repeat md:h-1/4 h-48 flex items-center justify-center relative md:w-full w-1/2 transition-transform duration-500 ease-in-out transform hover:scale-105 cursor-pointer hover:z-10'
+      className='group bg-cover bg-center bg-no-repeat md:h-1/4 h-48 flex items-center justify-center relative md:w-full w-1/2 md:transition-transform md:duration-500 md:ease-in-out md:transform hover:scale-105 cursor-pointer md:hover:z-10'
     >
       <div className='flex flex-col items-center justify-end h-full p-4'>
         <img src={logo} alt="Logo" className='h-24' />
