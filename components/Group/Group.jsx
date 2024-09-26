93dde4365ea0249c6c6c79545afa2b0252797fe3
Jimena Hernández <her21199@uvg.edu.gt>
2024-09-26 12:31:46 -0600

    Update Group.jsx

--- components/Group/Group.jsx
+++ components/Group/Group.jsx
@@ -1,11 +1,15 @@
+"use client"
 import React from 'react'
 import { FaPlus } from "react-icons/fa";
+import { useRouter } from 'next/navigation';
 
-const Group = ({ cover, logo, secondary }) => {
+const Group = ({ cover, logo, secondary,link }) => {
+  const router = useRouter();
   return (
     <div
       style={{ backgroundImage: `url(${cover})` }}
       className='group_container animation shadow-2xl'
+      onClick={() => router.push(link)}
     >
       <div className='flex flex-col items-center justify-end h-full py-8'>
         <img src={logo} alt="Logo" className='h-20' />
