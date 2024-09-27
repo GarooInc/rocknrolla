0edeb1ff5aeb7540b4fd3c7e946ad45777bdcaec
Jimena Hernández <her21199@uvg.edu.gt>
2024-09-27 12:01:14 -0600

    Update CardFace.jsx

--- components/CardFace/CardFace.jsx
+++ components/CardFace/CardFace.jsx
@@ -6,7 +6,7 @@ const CardFace = ({ face, card }) => {
     const [isFlipped, setIsFlipped] = useState(false);
 
     return (
-        <div className={`flex justify-center items-center relative transition-all duration-300 ease-out transform ${isFlipped ? 'md:w-[600px] scale-105 w-full' : 'md:w-[300px] scale-95 w-1/2'}`}>
+        <div className={`flex justify-center items-center relative transition-all duration-300 ease-out transform ${isFlipped ? 'md:w-[600px] md:scale-105 w-full' : 'md:w-[300px] md:scale-95 w-1/2'}`}>
             <img src={isFlipped ? card : face} alt='Line' className="object-cover w-full h-full"/>
             <div className='absolute top-4 right-4'>
                 {isFlipped ? (
