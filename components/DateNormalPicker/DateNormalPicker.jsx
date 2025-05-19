57f0c84de49e9d369126e4a6933a185f630a8d62
Jimena Hernández <her21199@uvg.edu.gt>
2025-05-19 08:36:15 -0600

    Update DateNormalPicker.jsx

--- components/DateNormalPicker/DateNormalPicker.jsx
+++ components/DateNormalPicker/DateNormalPicker.jsx
@@ -6,16 +6,12 @@ export default function CustomDatePicker({ value, onChange, label = "Date" }) {
     const dateValue = dayjs(e.target.value);
     onChange(dateValue);
   };
-
   return (
-    <div className='w-full'>
       <input
         className='bg-gray-200 rounded-2xl px-4 py-2 w-full datefield'
         type="date"
         value={dayjs(value).format('DD-MM-YYYY')}
         onChange={handleChange}
-        
       />
-    </div>
   );
 }
